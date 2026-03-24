5b482d63b8f54e2db8fd35d2cca2a0105eee642b
baebae6ae <baebae6ae@naver.com>
2026-03-24 10:28:40 +0900

    railway 배포용으로 수정

--- app.py
+++ app.py
@@ -442,5 +442,9 @@ def api_intervals():
     })
 
 
+# 수정 (Railway에서 작동함)
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
